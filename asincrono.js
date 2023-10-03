9d9e9f90957c60a0715196b87cb6a3869efbdf5c
KarenAlvere <alvarezpalafoxanakaren@gmail.com>
2023-10-03 15:01:31 -0600

    01 Asincronismo JavaScript

---
+++ asincrono.js
@@ -0,0 +1,20 @@
+const teams = ["Liverpool", "Dortmun", "Chelsie"];
+
+setTimeout(() => console.log('Tarea asincrona'));
+
+console.log('Sigue ejecutando');
+console.log(teams);
+
+function getTeams(){
+    return teams;
+}
+
+function asincGeTeams(){
+    setTimeout(() => teams, 2000);
+}
+
+console.log(getTeams());
+console.log('Final de código');
+
+console.log(getTeams());
+console.log('Termina el llamado de función')
